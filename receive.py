ba5460ab80a23e4feeb070413459c0a02435c23b
daichikuwa0618 <31601805+daichikuwa0618@users.noreply.github.com>
2017-09-20 16:20:27 +0900

    タプルからリストへ再変更・条件分岐の場所を変更・インデントの追加
    タプルは値の変更ができないため

--- receive.py
+++ receive.py
@@ -17,7 +17,7 @@ S7 = 16     # 右下3
 S8 = 20     # 右下2
 S9 = 21     # 右下1
 S10 = #動作していないという信号(Highで動作していない)
-chan_list = (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10)
+chan_list = [S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10]
 
 BIT_TO_RAD = 360/256    # bit情報から角度に変換
 RANGE = 2               # bit情報から距離に変換(2bit左シフト)
@@ -25,7 +25,7 @@ RANGE = 2               # bit情報から距離に変換(2bit左シフト)
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(S0, GPIO.OUT)
-    GPIO.setup(chan_list, GPIO.IN)
+    GPIO.setup(chan_list[1:12], GPIO.IN)
 
 def signalInput(signal):
     for cnt in range(8):
@@ -36,24 +36,24 @@ def signalInput(signal):
 
 def signalGet():
     try:
-        distance = angle = np.zeros(8, dtype=np.int)
-        GPIO.output(S0, 1)
-        GPIO.wait_for_edge(S1, GPIO.RISING)
-        signalInput(distance)
-        GPIO.wait_for_edge(S1, GPIO.FALLING)
-        signalInput(angle)
-        GPIO.output(S0, 0)
-        distance = "".join(map(str, distance))
-        angle = "".join(map(str, angle))
-        result = [int(distance,2)<<RANGE, int(angle,2)*BIT_TO_RAD]
         # 正常に動作している時に値を返す
         if not GPIO.input(chan_list[10]):
+            distance = angle = np.zeros(8, dtype=np.int)
+            GPIO.output(S0, 1)
+            GPIO.wait_for_edge(S1, GPIO.RISING)
+            signalInput(distance)
+            GPIO.wait_for_edge(S1, GPIO.FALLING)
+            signalInput(angle)
+            GPIO.output(S0, 0)
+            distance = "".join(map(str, distance))
+            angle = "".join(map(str, angle))
+            result = [int(distance,2)<<RANGE, int(angle,2)*BIT_TO_RAD]
             return result
 
     except Exception as e:
-    pass
+        pass
     else:
-    pass
+        pass
     finally:
-    GPIO.remove_event_detect(S1)
-    GPIO.cleanup()
+        GPIO.remove_event_detect(S1)
+        GPIO.cleanup()
